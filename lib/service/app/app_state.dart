9a178955b8316ab0440b06a9b7e23c7375a39cae
bluekmky1 <115731932+bluekmky1@users.noreply.github.com>
2024-09-12 23:13:50 +0900

    🎨 임시 로그인 가족 구서원 모집 함수 추가

---
+++ lib/service/app/app_state.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+/// 전역으로 관리하는 상태
+class AppState extends Equatable {
+  const AppState({
+    required this.isSignedIn,
+    required this.hasFamily,
+  });
+
+  const AppState.init()
+      : isSignedIn = true,
+        hasFamily = false;
+
+  final bool isSignedIn;
+  final bool hasFamily;
+
+  AppState copyWith({
+    bool? isSignedIn,
+    bool? hasFamily,
+  }) =>
+      AppState(
+          isSignedIn: isSignedIn ?? this.isSignedIn,
+          hasFamily: hasFamily ?? this.hasFamily);
+
+  @override
+  List<Object?> get props => <Object?>[isSignedIn];
+}
